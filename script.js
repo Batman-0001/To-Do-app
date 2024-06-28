9838f21a9bfef226f1ffa0e12b563fcb8042ae6f
Batman@001 <santrasuraj14@gmail.com>
2024-06-28 19:57:06 +0530

    add the updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //adding the toggle feature in the website
 const themeToggle = document.querySelector('.theme-toggle');
 const themeIcon = document.querySelector('.theme-icon');
-const bgImage = document.querySelector('.bg-image');
+
 
 themeToggle.addEventListener('click', () => {
   //selecting the specific class by addition of which toggle will occur
@@ -11,12 +11,10 @@ themeToggle.addEventListener('click', () => {
   if (isDark) {
     themeIcon.src = './images/icon-sun.svg';
     themeIcon.alt = 'dark-mode';
-    bgImage.style.backgroundImage = 'url("./images/bg-desktop-dark.jpg")';
 
   } else {
     themeIcon.src = './images/icon-moon.svg';
     themeIcon.alt = 'light-mode';
-    bgImage.style.backgroundImage = 'url("./images/bg-desktop-light.jpg")';
   }
 
 });
